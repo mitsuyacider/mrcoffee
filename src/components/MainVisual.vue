a312fa3be0b7219d6303cd5742c61034a60463b7
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-10-03 17:12:43 +0900

    Emit border in some tags to make the size and structure clear

--- src/components/MainVisual.vue
+++ src/components/MainVisual.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- メインビジュアル -->
   <div class="py-4">
-    <div class="container">
+    <div class="container border">
       <!-- カルーセル 外枠 -->
       <div id="main_visual" class="carousel slide" data-ride="carousel">
         <!-- インジケーター -->
-        <ol class="carousel-indicators">
+        <ol class="carousel-indicators border">
           <li data-target="#main_visual" data-slide-to="0" class="active"></li>
           <li data-target="#main_visual" data-slide-to="1"></li>
           <li data-target="#main_visual" data-slide-to="2"></li>
@@ -16,7 +16,7 @@
           <!-- スライド01 -->
           <div class="carousel-item active">
             <img src="../assets/slide_01.jpg" class="img-fluid" alt="コーヒー写真">
-            <div class="carousel-caption d-none d-md-block">
+            <div class="carousel-caption d-none d-md-block border">
               <h2>Mr.M COFFEEのこだわり</h2>
               <p>店主が世界中のコーヒー豆を厳選し、コーヒー豆の種類にあわせ、心を込めて焙煎、抽出しております。</p>
             </div>
@@ -40,11 +40,11 @@
         </div><!-- /カルーセル内枠 -->
 
         <!-- コントローラー -->
-        <a href="#main_visual" class="carousel-control-prev" role="button" data-slide="prev">
+        <a href="#main_visual" class="carousel-control-prev border" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">前に戻る</span>
         </a>
-        <a href="#main_visual" class="carousel-control-next" role="button" data-slide="next">
+        <a href="#main_visual" class="carousel-control-next border" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">次に進む</span>
         </a><!-- コントローラー -->
--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -2,21 +2,21 @@
   <!-- コンテンツ01 -->
   <div class="py-4">
     <section id="news">
-      <div class="container">
+      <div class="container border">
         <div class="row">
           <!-- 左側カラム（画面幅md以上) -->
-          <div class="col-md-2">
+          <div class="col-md-2 border">
             <h3 class="text-left">News</h3>
           </div>
           <!-- 右側カラム（画面幅md以上) -->
-          <div class="col-md-10 text-left">
+          <div class="col-md-10 text-left border">
             <dl class="row">
-              <dt class="col-md-3">2017年●月●日</dt>
-              <dd class="col-md-9">ランチクーポン配布中です</dd>
-              <dt class="col-md-3">2017年●月●日</dt>
-              <dd class="col-md-9">季節目メニューを追加しました</dd>
-              <dt class="col-md-3">2017年●月●日</dt>
-              <dd class="col-md-9">新しい雑貨さん入荷しました</dd>
+              <dt class="col-md-3 border">2017年●月●日</dt>
+              <dd class="col-md-9 border">ランチクーポン配布中です</dd>
+              <dt class="col-md-3 border">2017年●月●日</dt>
+              <dd class="col-md-9 border">季節目メニューを追加しました</dd>
+              <dt class="col-md-3 border">2017年●月●日</dt>
+              <dd class="col-md-9 border">新しい雑貨さん入荷しました</dd>
             </dl>
           </div>
         </div>
