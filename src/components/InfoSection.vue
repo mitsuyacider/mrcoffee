0091ea4846183e8f084f084dff8f0610e5bd7d26
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-10-04 17:28:44 +0900

    Add information component

--- src/components/InfoSection.vue
+++ src/components/InfoSection.vue
@@ -1,32 +1,63 @@
 <template>
   <!-- コンテンツ05 -->
-  <div class="py-4">
-    <section id="information">
-      <div class="container ">
+  <div class="py-4 border">
+    <section id="information" class="border">
+      <div class="container border">
         <h3 class="mb-3">Information</h3>
         <p>
           カフェMr M COFFEE（ミスターエムコーヒーは、山の中にあります。
           大自然に囲まれて、こだわりのコーヒーを飲みながら、美味しい空気と
           美しい景色をご堪能ください。
         </p>
-        <div class="row">
+        <div class="row border">
           <!-- 左側セクション -->
-          <div class="col-md-6">
+          <div class="col-md-6 border">
             <section id="shop">
               <h4 class="mb-3">Shop</h4>
               <!-- 店舗情報の表 -->
-              店舗情報の表が入ります。
-              <!-- /店舗情報の表 -->
+              <table class="table table-hover">
+                <tbody class="text-justify">
+                  <tr>
+                    <th>店名</th>
+                    <td>Mr.M COFFEE</td>
+                  </tr>
+                  <tr>
+                    <th>住所</th>
+                    <td>〒000-000 xx県xx市xx町123</td>
+                  </tr>
+                  <tr>
+                    <th>電話番号</th>
+                    <td>000-0000-0000</td>
+                  </tr>
+                  <tr>
+                    <th>営業時間</th>
+                    <td>8:00 ~ 18:00</td>
+                  </tr>
+                  <tr>
+                    <th>モーニング</th>
+                    <td>8:00 ~ 11:00</td>
+                  </tr>
+                  <tr>
+                    <th>定休日</th>
+                    <td>水曜日、不定休</td>
+                  </tr>
+                  <tr>
+                    <th>クレジットカード</th>
+                    <td>利用不可</td>
+                  </tr>
+                </tbody>
+              </table><!-- /店舗情報の表 -->
             </section><!-- 左側セクション -->
           </div>
 
           <!-- 右側セクション -->
-          <div class="col-md-6">
+          <div class="col-md-6 border">
             <section id="access">
               <h4 class="mb-3">Access</h4>
               <!-- アクセスマップ -->
-              アクセスマップが入ります
-              <!-- /アクセスマップが入ります -->
+              <div class="embed-responsive embed-responsive-4by3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d416726.1732753321!2d136.2542548857749!3d35.31410015363348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6003b6156d6ce99d%3A0xa76708554fc15ced!2z5bKQ6Zic55yM5aSn5Z6j5biC!5e0!3m2!1sja!2sjp!4v1538641306832" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div><!-- /アクセスマップが入ります -->
               <p>駅から徒歩12分（950m)、駐車場あり</p>
             </section>
           </div><!-- /右側セクション -->
